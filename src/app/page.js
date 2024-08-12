cc6b4ac7797f5af3fda0bbfabad07d55e37d4368
Anupama Singh <anupama12sg@gmail.com>
2024-08-13 00:01:34 +0530

    feat: Added delete and add task.

--- src/app/page.js
+++ src/app/page.js
@@ -5,11 +5,48 @@ import React, { useState } from "react";
 const Page = () => {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
+  const [mainTask, setMainTask] = useState([])
+  const submitHandler = (e) => {
+    e.preventDefault()
+    console.log(e)
+    setMainTask([...mainTask, { title, description }])
+    setTitle("")
+    setDescription("")
+    console.log(mainTask)
+  }
+
+  const deleteHandler = (i) => {
+    let copytask = [...mainTask]
+    copytask.splice(i, 1)
+    setMainTask(copytask)
+  }
+
+  let renderTask = <h2>No Task Available!!</h2>
+
+  if (mainTask.length > 0) {
+    renderTask = mainTask.map((t, i) => {
+      return (
+        <li key={i} className="flex items-center justify-between">
+          <div className="flex justify-between mb-5 w-2/3 ">
+            <h5 className="text-2xl font-semibold">{t.title}</h5>
+            <h6 className="text-lg font-medium">{t.description}</h6>
+          </div>
+          <button
+            onClick={()=>{
+              deleteHandler(i)
+            }}
+            className="bg-red-400 text-white px-4 py-2 rounded font-bold">
+            Delete Task
+          </button>
+        </li>
+      )
+    });
+  }
 
   return (
     <>
-      <h1 className="bg-sky-500 p-10 text-5xl font-bold text-center">My To-Do List</h1>
-      <form>
+      <h1 className="bg-sky-500 p-10 text-5xl font-bold text-center">Anu's To-Do List</h1>
+      <form onSubmit={submitHandler}>
         <input
           type="text"
           className="text-black border-zinc text-center border-4 m-8 px-2 py-2"
@@ -32,6 +69,12 @@ const Page = () => {
           Add Task
         </button>
       </form>
+      <hr />
+      <div className="p-8 bg-purple-300">
+        <ul>
+          {renderTask}
+        </ul>
+      </div>
     </>
   );
 };
